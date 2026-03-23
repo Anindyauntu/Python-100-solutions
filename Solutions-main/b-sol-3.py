45431f73bd9369dd4ceddaa5a97f3c8ddde48ec3
Anindyauntu <anindya.auntu@gmail.com>
2026-03-23 12:11:48 +0600

    Pro-3

---
+++ Solutions-main/b-sol-3.py
@@ -0,0 +1,8 @@
+def addition(num1,num2):
+    add_result = num1+num2
+    print("The addition of two numbers is",add_result)
+
+user_input1 = int(input("Enter the first number:"))
+user_input2 = int(input("Enter the second number:"))
+
+addition(user_input1,user_input2)
