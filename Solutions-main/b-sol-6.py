953f3344b8dbe73e74b31105dd507a93579b4c1e
Anindyauntu <anindya.auntu@gmail.com>
2026-03-24 11:37:13 +0600

    Pro-6

---
+++ Solutions-main/b-sol-6.py
@@ -0,0 +1,9 @@
+# Swapping the variables (VALUES)
+
+first_variable = 67
+second_variable = 33
+
+first_variable, second_variable = second_variable, first_variable
+
+print(first_variable)
+print(second_variable)
